61dd1fc10b3c43380b871d45a5f50a6aed958a75
Ranhuiryan <qdgjhrh@outlook.com>
2024-12-25 11:28:27 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const webpack = require('webpack');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   
   return {
     ...config,
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        'process.env.EXPO_ROUTER_APP_ROOT': JSON.stringify('./app')
+      })
+    ],
     resolve: {
       ...config.resolve,
       alias: {
